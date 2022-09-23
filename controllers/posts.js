508289359c1c294ba61f89511bcb33fb81501849
resilient-coders35 <calvinbensonj@gmail.com>
2022-09-23 18:06:34 -0400

    paths

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,6 +21,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).populate('user').lean();
+      // const comments = await Comment.find({ post: req.params.id}).sort({ createdAt: desc}).lean();
       res.render("post.ejs", { post: post, user: req.user });
     } catch (err) {
       console.log(err);
---
+++ models/Comment.js
@@ -0,0 +1,28 @@
+// Comments Model
+
+const mongoose = require("mongoose")
+
+const CommentSchema = new mongoose.Schema({
+    comment: {
+        type: String.
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User" ,
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema)
